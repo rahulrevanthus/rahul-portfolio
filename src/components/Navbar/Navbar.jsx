7a45cfc13be5e99cdac41cdde9487c353f1b2af9
rahulrevanthus <rahulrevanthus@gmail.com>
2025-03-19 02:35:04 -0500

    updated navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,30 @@
 import { useState } from "react";
 import styles from "./Navbar.module.css";
-import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/">
-        Portfolio
-      </a>
+      <a className={styles.title} href="/">Portfolio</a>
       <div className={styles.menu}>
+        <span
+          className={styles.menuBtn}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? "Close" : "Menu"}
+        </span>
         <ul
           className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
           onClick={() => setMenuOpen(false)}
         >
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#experience">Experience</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#projects">Projects</a></li>
           <li className={styles.contactItem}>
             <a href="#contact">Contact</a>
-            
           </li>
-          <li>
-            <a href="#contact">Resume</a> {/* Redirects to Contact */}
-          </li>
+          <li><a href="#contact">Resume</a></li>
         </ul>
       </div>
     </nav>
